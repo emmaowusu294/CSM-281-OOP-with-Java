80e749cc1dad1df45df972f6955ce21c06e4dcac
4Ts-code <eo8407272@gmail.com>
2025-02-13 16:13:15 +0000

    Loops

---
+++ Evaluations/Loops.java
@@ -0,0 +1,19 @@
+package Evaluations;
+
+public class Loops {
+    public static void main(String[] args)
+    {
+     int i;
+
+     for (i = 2; i > 25; i-- )
+        {
+            if (i == 15) break;
+          
+            System.out.println(i *= 2); 
+          
+            
+        }
+
+
+    }
+}
