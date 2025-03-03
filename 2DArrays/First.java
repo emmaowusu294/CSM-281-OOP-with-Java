fbd01ad82a86b9092a3ffdc7f6360ecd1a6bcb26
4Ts-code <eo8407272@gmail.com>
2025-03-03 18:28:02 +0000

    2D Arrays

---
+++ 2DArrays/First.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class First {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int[] total = new int[5];
+
+        int b[][] = {{1, 2, 4, 7, 6}, {3, 4, 5, 9, 0}};
+
+        // Summing the columns
+        for (int i = 0; i < b.length; i++) {
+            for (int j = 0; j < b[i].length; j++) {  // Fixed 'lenght' to 'length'
+                total[j] += b[i][j];  // Summing values column-wise
+            }
+        }
+
+        // Printing total array
+        System.out.println("Column-wise sums:");
+        for (int i = 0; i < total.length; i++) {
+            System.out.println("Column " + i + ": " + total[i]);
+        }
+
+        input.close();  // Close the scanner
+    }
+}
